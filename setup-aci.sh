8fb64459cff89a4f3565763a31d60f682edfdc23
Suraj Deshmukh <surajd.service@gmail.com>
2024-08-09 16:03:25 -0700

    Use the cloudshell tools image

--- setup-aci.sh
+++ setup-aci.sh
@@ -2,7 +2,7 @@
 
 export AZURE_RESOUCE_GROUP=suraj-aci
 export AZURE_LOCATION=eastus
-export AZURE_CONTAINER_GROUP=foobar
+export AZURE_CONTAINER_GROUP=cloudshelltools
 
 az group create \
     --name "${AZURE_RESOUCE_GROUP}" \
@@ -11,5 +11,7 @@ az group create \
 az container create \
     --resource-group "${AZURE_RESOUCE_GROUP}" \
     --name "${AZURE_CONTAINER_GROUP}" \
-    --image quay.io/surajd/az-cli:latest \
+    --image quay.io/surajd/cloudshell-tools:master-e060fc5-2024-08-Aug-09-07-46-19 \
     --command-line "/bin/sh -c 'sleep infinity'"
+
+az container exec -g ${AZURE_RESOUCE_GROUP} --name ${AZURE_CONTAINER_GROUP} --container-name cloudshelltools --exec-command "/bin/bash"
